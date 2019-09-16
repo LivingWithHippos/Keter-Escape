f968cacb8b7951461f5397d3f8757ae5c545a4f8
ScientiaSitPotentia <LivingWithHippos@users.noreply.github.com>
2019-09-16 19:30:07 +0200

    Added Constant file
    Keep here your app's constants

---
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/utilities/Constants.kt
@@ -0,0 +1,7 @@
+package com.onewisebit.scp_scarycontainmentpanic.utilities
+
+/**
+ * Constants used throughout the app.
+ */
+const val DATABASE_NAME = "SCP.db"
+const val ROLE_DATA_FILENAME = "roles.json"
