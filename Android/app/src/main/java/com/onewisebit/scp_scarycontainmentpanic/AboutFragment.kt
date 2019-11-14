0878377ea81227b4011942ed4dd777d2a7add1d4
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-11-14 19:30:11 +0100

    Updated About Screen
    showing a couple of textview now, to be updated

--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/AboutFragment.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/AboutFragment.kt
@@ -16,7 +16,7 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // TODO: populate the fragment with credits and such
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
