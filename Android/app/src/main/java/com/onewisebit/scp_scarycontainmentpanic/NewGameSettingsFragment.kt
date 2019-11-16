5c28c7de531faf48997c0419eb644ceee2cbb7f1
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-11-16 11:17:17 +0100

    Added configuration fragment for new games

---
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/NewGameSettingsFragment.kt
@@ -0,0 +1,30 @@
+package com.onewisebit.scp_scarycontainmentpanic
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.onewisebit.scp_scarycontainmentpanic.databinding.FragmentNewGameSettingsBinding
+
+class NewGameSettingsFragment:Fragment() {
+
+    private lateinit var binding: FragmentNewGameSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNewGameSettingsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.playerPicker.minValue=3
+        binding.playerPicker.maxValue=10
+
+    }
+}
