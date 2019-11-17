93f1856170a1f07e65d2958e1ac5afaa65665ebd
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-11-17 18:01:48 +0100

    Fixed column names

--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/model/Participant.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/model/Participant.kt
@@ -4,20 +4,20 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 
-@Entity(primaryKeys = ["gameID","playerID"],
+@Entity(primaryKeys = ["game","player"],
     tableName = "participants",
     foreignKeys = arrayOf(
         ForeignKey(entity = Game::class,
-            parentColumns = ["id"],
+            parentColumns = ["gameID"],
             childColumns = ["game"],
             //TODO: implement logic to manage the deletion of a player with a saved/running game
             onDelete = ForeignKey.NO_ACTION),
         ForeignKey(entity = Player::class,
-            parentColumns = ["id"],
+            parentColumns = ["playerID"],
             childColumns = ["player"],
             onDelete = ForeignKey.NO_ACTION),
         ForeignKey(entity = Role::class,
-            parentColumns = ["name"],
+            parentColumns = ["rolename"],
             childColumns = ["role"],
             onDelete = ForeignKey.NO_ACTION)
     ))
