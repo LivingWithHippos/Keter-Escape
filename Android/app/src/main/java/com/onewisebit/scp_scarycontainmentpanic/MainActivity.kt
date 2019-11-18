bd3b0691491650e8f2b230d17aae969ed72dff0b
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-11-18 18:04:00 +0100

    Added viewbinding to main activity

--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/MainActivity.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/MainActivity.kt
@@ -2,15 +2,19 @@ package com.onewisebit.scp_scarycontainmentpanic
 
 import android.os.Bundle
 import com.onewisebit.scp_scarycontainmentpanic.StartContract.StartView
+import com.onewisebit.scp_scarycontainmentpanic.databinding.ActivityMainBinding
 import com.onewisebit.scp_scarycontainmentpanic.presenters.StartActivityPresenter
 
 class MainActivity : BaseSCPActivity(), StartView {
 
     private var presenter: StartActivityPresenter? = null
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         presenter = StartActivityPresenter(this, applicationContext)
     }
