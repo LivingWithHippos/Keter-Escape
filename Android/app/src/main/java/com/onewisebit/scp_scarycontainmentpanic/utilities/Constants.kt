ccaffc6bc9bd7cb72fb193e6fc3c983932c1b0a8
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-11-20 11:10:28 +0100

    Added game type keyword

--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/utilities/Constants.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/utilities/Constants.kt
@@ -11,6 +11,7 @@ const val DEFAULT_THEME = "theme_foundation"
 const val CURRENT_THEME = "preference_current_theme"
 
 
+const val GAME_TYPE = "game_type"
 const val GAME_TYPE_PASS = 0
 
 const val GAME_CLASSIC_MIN_PLAYERS = 7
