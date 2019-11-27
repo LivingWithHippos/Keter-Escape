715af618652f0c1733cda1034d6a96b5c2e79fbb
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-11-27 22:41:55 +0100

    Update SCPModule.kt
    added daos
    removed unused imports

--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/SCPModule.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/SCPModule.kt
@@ -2,12 +2,18 @@ package com.onewisebit.scp_scarycontainmentpanic
 
 import android.content.Context
 import android.content.SharedPreferences
-import com.onewisebit.scp_scarycontainmentpanic.model.GameRepository
-import com.onewisebit.scp_scarycontainmentpanic.model.InGameRepository
+import com.onewisebit.scp_scarycontainmentpanic.model.SCPDatabase
 import com.onewisebit.scp_scarycontainmentpanic.utilities.PREF_FILE
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
     single<SharedPreferences> { androidContext().getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE)}
+
+    single {SCPDatabase.getInstance(get())}
+    single {get<SCPDatabase>().playerDAO()}
+    single {get<SCPDatabase>().roleDAO()}
+    single {get<SCPDatabase>().gameDAO()}
+    single {get<SCPDatabase>().modeDAO()}
+    single {get<SCPDatabase>().participantDAO()}
 }
