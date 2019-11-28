bf998e8b2db310dc37dab3745e0938a8dd86f17b
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-11-28 17:57:16 +0100

    Update ParticipantsChoiceFragment.kt
    linked to mvp

--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/ParticipantsChoiceFragment.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/ParticipantsChoiceFragment.kt
@@ -8,12 +8,18 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.onewisebit.scp_scarycontainmentpanic.databinding.FragmentParticipantsChoiceBinding
 import com.onewisebit.scp_scarycontainmentpanic.model.Player
+import com.onewisebit.scp_scarycontainmentpanic.model.PlayersModelImpl
+import com.onewisebit.scp_scarycontainmentpanic.presenters.PlayersPresenterImpl
+import org.koin.android.ext.android.inject
 
-class ParticipantsChoiceFragment:Fragment() {
+class ParticipantsChoiceFragment : Fragment(), PlayersContract.PlayersView {
 
     private lateinit var gridLayoutManager: GridLayoutManager
     private lateinit var adapter: ParticipantsAdapter
     private lateinit var binding: FragmentParticipantsChoiceBinding
+    private lateinit var presenter: PlayersContract.PlayersPresenter
+    //TODO: check if the model needs to be the interface and not the implementation
+    private val model: PlayersModelImpl by inject()
 
     private var playersList: ArrayList<Player> = ArrayList()
 
@@ -23,7 +29,12 @@ class ParticipantsChoiceFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentParticipantsChoiceBinding.inflate(layoutInflater)
-        gridLayoutManager = GridLayoutManager(this.context,2)
+        gridLayoutManager = GridLayoutManager(this.context, 2)
+        presenter = PlayersPresenterImpl(this,model)
         return binding.root
+    }
+
+    override fun initView(players: ArrayList<Player>) {
+        playersList = players
     }
 }
