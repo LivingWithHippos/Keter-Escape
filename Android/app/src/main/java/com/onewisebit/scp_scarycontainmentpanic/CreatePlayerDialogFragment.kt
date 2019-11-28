1789ea6fb847d91e36ba334468bbd51ca3199fad
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-11-28 20:48:24 +0100

    Added Player's creation dialog

---
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/CreatePlayerDialogFragment.kt
@@ -0,0 +1,55 @@
+package com.onewisebit.scp_scarycontainmentpanic
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.onewisebit.scp_scarycontainmentpanic.databinding.DialogCreatePlayerBinding
+
+class CreatePlayerDialogFragment : DialogFragment() {
+
+    private lateinit var binding: DialogCreatePlayerBinding
+
+    internal lateinit var listener: NewPlayerDialogListener
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+
+            val inflater = requireActivity().layoutInflater;
+            binding = DialogCreatePlayerBinding.inflate(inflater)
+
+            builder.setView(binding.root)
+                .setTitle(R.string.new_player)
+                .setPositiveButton(
+                    R.string.save
+                ) { _, _ ->
+                    listener.onPositiveDialogClick(binding.etNewPlayerName.text.toString())
+                }
+                .setNegativeButton(
+                    R.string.cancel
+                ) { _, _ ->
+                    // For now do nothing on cancel
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            listener = context as NewPlayerDialogListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException(
+                (context.toString() +
+                        " must implement NewPlayerDialogListener")
+            )
+        }
+    }
+
+    interface NewPlayerDialogListener {
+        //TODO: also pass avatar bitmap link
+        fun onPositiveDialogClick(playerName: String)
+    }
+}
