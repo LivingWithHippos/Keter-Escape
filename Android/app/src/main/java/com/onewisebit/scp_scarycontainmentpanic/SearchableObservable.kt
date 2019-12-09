b4d42d82988c9c0fa1aed30a94d57247a2a52d1c
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-12-09 15:33:24 +0100

    Created SearchableObservable class
    paired with the searchView to manage search query

---
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/SearchableObservable.kt
@@ -0,0 +1,30 @@
+package com.onewisebit.scp_scarycontainmentpanic
+
+import android.widget.SearchView
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+
+class SearchableObservable {
+
+    fun fromView(searchView: SearchView): Observable<String> {
+
+        val subject : PublishSubject<String> = PublishSubject.create()
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /**
+             * Returning true means we have managed the event
+             */
+            override fun onQueryTextSubmit(s: String): Boolean {
+                subject.onNext(s)
+                return true
+            }
+
+            override fun onQueryTextChange(text: String): Boolean {
+                subject.onNext(text)
+                return true
+            }
+        })
+
+        return subject
+    }
+}
