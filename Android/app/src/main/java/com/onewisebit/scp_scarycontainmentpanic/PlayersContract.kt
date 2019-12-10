b5942ba51ab77c318a482fd73ef9e2dd3a541c76
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-12-10 10:32:04 +0100

    Updated playerscontract
    Use newly added methods to manage participants

--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/PlayersContract.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/PlayersContract.kt
@@ -1,20 +1,26 @@
 package com.onewisebit.scp_scarycontainmentpanic
 
 import com.onewisebit.scp_scarycontainmentpanic.model.Player
+import io.reactivex.Completable
 import io.reactivex.Flowable
 
 interface PlayersContract {
 
     interface PlayersView {
-        fun initView(players: Flowable<List<Player>>)
+        fun initView(players: Flowable<List<Player>>, participants: Flowable<List<Long>>)
     }
 
     interface PlayersPresenter {
-        fun setPlayers()
+        fun setPlayers(gameID:Long)
+        fun addParticipant(gameID: Long, playerID: Long): Completable
+        fun removeParticipant(gameID: Long, playerID: Long)
     }
 
     interface PlayersModel {
         fun getAllPlayers(): Flowable<List<Player>>
         fun getPlayersByName(name: String): Flowable<List<Player>>
+        fun getParticipantsByGame(gameID: Long): Flowable<List<Long>>
+        fun addGameParticipant(gameID: Long, playerID: Long): Completable
+        fun removeGameParticipant(gameID: Long, playerID: Long)
     }
 }
--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/model/PlayersModelImpl.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/model/PlayersModelImpl.kt
@@ -1,18 +1,31 @@
 package com.onewisebit.scp_scarycontainmentpanic.model
 
 import com.onewisebit.scp_scarycontainmentpanic.PlayersContract
+import io.reactivex.Completable
 import io.reactivex.Flowable
 
-class PlayersModelImpl(playerRepository: PlayerRepository):PlayersContract.PlayersModel {
+class PlayersModelImpl(playerRepository: PlayerRepository,participantRepository: ParticipantRepository):PlayersContract.PlayersModel {
 
-    private var repository:PlayerRepository = playerRepository
+    private var playerRepo:PlayerRepository = playerRepository
+    private var participantRepo:ParticipantRepository = participantRepository
 
     override fun getAllPlayers(): Flowable<List<Player>> {
-     return repository.getAllPlayers()
+     return playerRepo.getAllPlayers()
     }
 
     override fun getPlayersByName(name: String): Flowable<List<Player>> {
-        return repository.getPlayersByName(name)
+        return playerRepo.getPlayersByName(name)
     }
 
+    override fun getParticipantsByGame(gameID: Long): Flowable<List<Long>> {
+        return participantRepo.getGameParticipantsID(gameID)
+    }
+
+    override fun addGameParticipant(gameID: Long, playerID: Long): Completable {
+        return participantRepo.insertParticipant(Participant(gameID,playerID,null,null))
+    }
+
+    override fun removeGameParticipant(gameID: Long, playerID: Long) {
+        return participantRepo.removeParticipant(gameID, playerID)
+    }
 }
