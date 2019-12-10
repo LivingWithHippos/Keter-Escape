577a10d0db4178e7efb7aa92b34146e44fe5d454
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-12-10 13:49:55 +0100

    Updated players choice list
    Viewholder are now different for selected players

--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/ParticipantsAdapter.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/ParticipantsAdapter.kt
@@ -7,37 +7,59 @@ import androidx.recyclerview.widget.RecyclerView
 import com.onewisebit.scp_scarycontainmentpanic.databinding.PlayerListItemBinding
 import com.onewisebit.scp_scarycontainmentpanic.model.Player
 import android.view.LayoutInflater
+import com.onewisebit.scp_scarycontainmentpanic.databinding.ParticipantListItemBinding
+import com.onewisebit.scp_scarycontainmentpanic.model.Participant
 
 
-class ParticipantsAdapter(playersList: List<Player>) :
-    RecyclerView.Adapter<ParticipantsAdapter.PlayerHolder>() {
+class ParticipantsAdapter(playersList: List<Player>, participantsID: List<Long>,
+                          private val clickListener: (Long,Boolean) -> Unit ) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var fullPlayers: List<Player> = playersList
     private var players: List<Player> = playersList
 
+    private var participants: List<Long> = participantsID
+
     private var nameFilter: String? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
         val layoutInflater = LayoutInflater.from(parent.context)
-        val playerBinding = PlayerListItemBinding.inflate(layoutInflater, parent, false)
-        return PlayerHolder(playerBinding)
+
+        when (viewType) {
+            TYPE_PARTICIPANT  -> {
+                val participantBinding = ParticipantListItemBinding.inflate(layoutInflater, parent, false)
+                return ParticipantHolder(participantBinding)
+            }
+            else -> {
+                val playerBinding = PlayerListItemBinding.inflate(layoutInflater, parent, false)
+                return PlayerHolder(playerBinding)
+            }
+        }
+
     }
 
     override fun getItemCount(): Int = players.size
 
-    override fun onBindViewHolder(holder: PlayerHolder, position: Int) {
-        val itemPlayer = players[position]
-        holder.bind(itemPlayer)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = players[position]
+        if (holder is ParticipantHolder)
+            holder.bind(item,clickListener)
+        if (holder is PlayerHolder)
+            holder.bind(item,clickListener)
     }
 
     /**
      * Set a list of players items.
      */
     fun setPlayers( newPlayers:List<Player>){
-        //TODO: maybe move this in the mvp structure somehow?
         fullPlayers = newPlayers
         updatePlayers()
+    }
+
+    fun setParticipants( ids:List<Long>){
+        participants = ids
+        notifyDataSetChanged()
     }
 
     fun setPlayersNameFilter(name: String){
@@ -53,28 +75,41 @@ class ParticipantsAdapter(playersList: List<Player>) :
         notifyDataSetChanged()
     }
 
+    override fun getItemViewType(position: Int): Int {
+        if ( participants.any{it == players[position].id} )
+            return TYPE_PARTICIPANT
+        return TYPE_PLAYER
+    }
 
     companion object {
         private val TAG = ParticipantsAdapter::class.java.simpleName
+        const val TYPE_PLAYER: Int = 0
+        const val TYPE_PARTICIPANT: Int = 1
     }
 
-    class PlayerHolder(pBinding: PlayerListItemBinding) : RecyclerView.ViewHolder(pBinding.root), View.OnClickListener {
+    class PlayerHolder(pBinding: PlayerListItemBinding) : RecyclerView.ViewHolder(pBinding.root) {
 
         private var player: Player? = null
         private val binding:PlayerListItemBinding =pBinding
 
-        init {
-            binding.cvPlayer.setOnClickListener(this)
-        }
-
-        fun bind(_player: Player) {
+        fun bind(_player: Player,_clickListener: (Long,Boolean) -> Unit) {
             player = _player
-            binding.tvPlayerName.text= player!!.name
+            binding.tvPlayerName.text= _player.name
+            binding.cvPlayer.setOnClickListener{_clickListener(_player.id,true)}
         }
 
-        override fun onClick(v: View?) {
-            //TODO: select player -> participant and change view
-            Log.d("PlayerHolder", "no touchie")
+    }
+
+
+    class ParticipantHolder(pBinding: ParticipantListItemBinding) : RecyclerView.ViewHolder(pBinding.root) {
+
+        private var player: Player? = null
+        private val binding:ParticipantListItemBinding = pBinding
+
+        fun bind(_player: Player,_clickListener: (Long,Boolean) -> Unit) {
+            player = _player
+            binding.tvParticipantName.text= _player.name
+            binding.cvParticipant.setOnClickListener{_clickListener(_player.id,false)}
         }
 
     }
--- Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/ParticipantsChoiceFragment.kt
+++ Android/app/src/main/java/com/onewisebit/scp_scarycontainmentpanic/ParticipantsChoiceFragment.kt
@@ -10,20 +10,16 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
 import com.onewisebit.scp_scarycontainmentpanic.databinding.FragmentParticipantsChoiceBinding
+import com.onewisebit.scp_scarycontainmentpanic.model.Participant
 import com.onewisebit.scp_scarycontainmentpanic.model.Player
-import com.onewisebit.scp_scarycontainmentpanic.model.PlayersModelImpl
-import com.onewisebit.scp_scarycontainmentpanic.presenters.PlayersPresenterImpl
 import io.reactivex.Flowable
-import io.reactivex.ObservableSource
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Consumer
-import io.reactivex.functions.Predicate
 import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 import java.util.concurrent.TimeUnit
-import io.reactivex.functions.Function
 
+@SuppressLint("CheckResult")
 class ParticipantsChoiceFragment : Fragment(), PlayersContract.PlayersView {
 
     private lateinit var layoutManager: GridLayoutManager
@@ -32,6 +28,8 @@ class ParticipantsChoiceFragment : Fragment(), PlayersContract.PlayersView {
     private val presenter: PlayersContract.PlayersPresenter by inject{ parametersOf(this) }
     private val args: ParticipantsChoiceFragmentArgs by navArgs()
 
+    //TODO: add diffutils
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,35 +37,50 @@ class ParticipantsChoiceFragment : Fragment(), PlayersContract.PlayersView {
     ): View? {
         binding = FragmentParticipantsChoiceBinding.inflate(layoutInflater)
         layoutManager = GridLayoutManager(this.context, 2)
-        adapter = ParticipantsAdapter(emptyList())
+        adapter = ParticipantsAdapter(emptyList(), emptyList()) { id: Long, add: Boolean ->playerToggle(id,add)}
         enablePlayerSearch()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //TODO: change remaining players as they're selected
-        binding.tvSelectPlayersTitle.text =
-            getString(R.string.select_players, args.totPlayers, args.totPlayers)
         binding.rvPlayers.layoutManager = layoutManager
         binding.rvPlayers.adapter = adapter
         binding.bCreatePlayer.setOnClickListener{
             showCreatePlayerDialog()
         }
-        presenter.setPlayers()
+        onParticipantUpdated()
+        presenter.setPlayers(args.gameID)
     }
 
-    @SuppressLint("CheckResult")
-    override fun initView(players: Flowable<List<Player>>) {
+    private fun onParticipantUpdated(){
+        presenter.getParticipantsNumber(args.gameID)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { binding.tvSelectPlayersTitle.text =
+                    getString(R.string.select_players, args.totPlayers, args.totPlayers-it) },
+                { Log.d(TAG, "Can't get Participants Number") }
+            )
+
+    }
+
+    override fun initView(players: Flowable<List<Player>>,participants: Flowable<List<Long>>) {
         players.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-            { adapter.setPlayers(it) },
-            { Log.d(TAG, "Players List retrieval error") }
-        )
+                { adapter.setPlayers(it) },
+                { Log.d(TAG, "Players List retrieval error") }
+            )
+
+        participants.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { adapter.setParticipants(it) },
+                { Log.d(TAG, "Participants List retrieval error") }
+            )
     }
 
-    @SuppressLint("CheckResult")
     private fun enablePlayerSearch(){
         SearchableObservable().fromView(binding.svPlayerFilter)
             //run only if input stops for 500 ms
@@ -77,6 +90,28 @@ class ParticipantsChoiceFragment : Fragment(), PlayersContract.PlayersView {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { text -> adapter.setPlayersNameFilter(text) }
+    }
+
+    private fun playerToggle(id:Long, add:Boolean){
+        if(add){
+            presenter.addParticipant(args.gameID,id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { Log.d(TAG, "Participant Insert Success") },
+                    { Log.d(TAG, "Participant Insert Error") }
+                )
+        }else{
+            presenter.removeParticipant(args.gameID,id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { Log.d(TAG, "Participant removal Success")
+                         },
+                    { Log.d(TAG, "Participant removal Error") }
+                )
+        }
+        onParticipantUpdated()
     }
 
     private fun showCreatePlayerDialog(){
