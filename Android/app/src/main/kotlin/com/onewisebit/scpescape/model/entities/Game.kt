fbdabc2056ffae180d0337461429903b4c513fea
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-12-11 21:58:45 +0100

    Added Mode link to Game entity
    updated db scheme

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/model/entities/Game.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/model/entities/Game.kt
@@ -2,13 +2,22 @@ package com.onewisebit.scpescape.model.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "games")
+@Entity(tableName = "games",
+    foreignKeys = [ForeignKey(
+        entity = Mode::class,
+        parentColumns = ["mode_ID"],
+        childColumns = ["mode"],
+        onDelete = ForeignKey.NO_ACTION
+    )])
 data class Game(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "game_ID")
     val id: Long,
+    @ColumnInfo(name = "mode")
+    val modeID: Long,
     @ColumnInfo(name = "game_type")
     val type: Int,
     //TODO: test this value
