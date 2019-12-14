d053f87224926d8278e2c3c44d236ffd8b7ca74f
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2019-12-14 20:52:14 +0100

    Added cleaning of temporary games
    also useful to ping db on first run

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/di/SCPApplication.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/di/SCPApplication.kt
@@ -1,12 +1,19 @@
 package com.onewisebit.scpescape.di
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.util.Log
+import com.onewisebit.scpescape.model.database.SCPDatabase
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class SCPApplication : Application() {
 
+    @SuppressLint("CheckResult")
     override fun onCreate() {
         super.onCreate()
 
@@ -18,5 +25,19 @@ class SCPApplication : Application() {
             // module list
             modules(appModule)
         }
+
+        //ping the db so it gets created on the first run
+        //TODO: test if it happens
+        val db: SCPDatabase by inject()
+        db.gameDAO().deleteTemporaryGames().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { Log.d(TAG, "Temporary games deleted successfully") },
+                { exc -> Log.d(TAG, "Error while deleting temporary games", exc) }
+            )
+    }
+
+    companion object {
+        private val TAG = SCPApplication::class.java.simpleName
     }
 }
