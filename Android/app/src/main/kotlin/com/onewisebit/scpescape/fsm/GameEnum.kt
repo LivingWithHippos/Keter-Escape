611c65af41afc8f31a63364ae059adf80d09548b
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-13 19:36:20 +0100

    Create GameEnum.kt
    lists different possible game states

---
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/GameEnum.kt
@@ -0,0 +1,14 @@
+package com.onewisebit.scpescape.fsm
+
+enum class GameEnum {
+    STATE_VOTE,
+    STATE_RESULTS,
+    STATE_START,
+    STATE_END,
+    STATE_PASS,
+    STATE_END_ROUND,
+    STATE_START_ROUND,
+    STATE_PROFILE,
+    //generic
+    STATE_STATIC_SCREEN
+}
