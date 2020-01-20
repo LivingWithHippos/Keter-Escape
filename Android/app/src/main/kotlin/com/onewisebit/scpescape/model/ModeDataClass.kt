5ddb8d8fa68bb4f3d4db44f43d8ce3b50c91433d
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-20 18:02:29 +0100

    Changed New Mode data class

---
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/model/ModeDataClass.kt
@@ -0,0 +1,24 @@
+package com.onewisebit.scpescape.model
+
+data class ModeDataClass(
+    val id: String,
+    val name: String,
+    val shortDescription: String,
+    val description: String,
+    val rules: String,
+    val min: String,
+    val max: String,
+    val rolesDivision: List<RolesDivision>
+)
+
+data class RolesDivision(
+    val minPlayers: Int,
+    val maxPlayers: Int,
+    val roles: List<Role>
+)
+
+data class Role(
+    val role: String,
+    val quantity: Int,
+    val default: Boolean
+)
