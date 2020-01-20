c824c2189192b6c6c4adff14f72c6d2d7a45c65c
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-20 18:12:28 +0100

    Added model and presenter skeleton for game modes fragment

---
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/modesList/model/GameModesModelImpl.kt
@@ -0,0 +1,12 @@
+package com.onewisebit.scpescape.modesList.model
+
+import com.onewisebit.scpescape.model.ModeDataClass
+import com.onewisebit.scpescape.modesList.GameModesContract
+
+class GameModesModelImpl: GameModesContract.GameModesModel {
+
+
+    override suspend fun getModes(): List<ModeDataClass> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/modesList/presenter/GameModesPresenterImpl.kt
@@ -0,0 +1,13 @@
+package com.onewisebit.scpescape.modesList.presenter
+
+import com.onewisebit.scpescape.modesList.GameModesContract
+
+class GameModesPresenterImpl(gView : GameModesContract.GameModesView, gModel: GameModesContract.GameModesModel) : GameModesContract.GameModesPresenter{
+
+    private var view  = gView
+    private var model   = gModel
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
