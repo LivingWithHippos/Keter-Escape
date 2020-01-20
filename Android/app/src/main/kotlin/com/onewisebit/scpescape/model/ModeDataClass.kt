7cb891e2e983bcc3434bce02f0b7015297c4b806
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-21 00:02:33 +0100

    Mode Data Class updated with serialized names

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/model/ModeDataClass.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/model/ModeDataClass.kt
@@ -1,24 +1,40 @@
 package com.onewisebit.scpescape.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ModeDataClass(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("short_description")
     val shortDescription: String,
+    @SerializedName("description")
     val description: String,
+    @SerializedName("rules")
     val rules: String,
-    val min: String,
-    val max: String,
+    @SerializedName("min")
+    val min: Int,
+    @SerializedName("max")
+    val max: Int,
+    @SerializedName("roles_division")
     val rolesDivision: List<RolesDivision>
 )
 
 data class RolesDivision(
+    @SerializedName("min_players")
     val minPlayers: Int,
+    @SerializedName("max_players")
     val maxPlayers: Int,
+    @SerializedName("roles")
     val roles: List<Role>
 )
 
 data class Role(
+    @SerializedName("role")
     val role: String,
+    @SerializedName("quantity")
     val quantity: Int,
+    @SerializedName("default")
     val default: Boolean
 )
