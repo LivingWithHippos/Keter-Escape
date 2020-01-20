36b0a2ea976b585c147ad3b9ba4270f90d1c133f
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-20 18:36:33 +0100

    replaced reference to old class

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/model/repositories/InModelNewRepository.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/model/repositories/InModelNewRepository.kt
@@ -1,9 +1,9 @@
 package com.onewisebit.scpescape.model.repositories
 
-import com.onewisebit.scpescape.model.ModeFromJson
+import com.onewisebit.scpescape.model.ModeDataClass
 
 interface InModelNewRepository {
 
-    suspend fun getMode(id: Int): ModeFromJson?
-    suspend fun getAllModes(): List<ModeFromJson>?
+    suspend fun getMode(id: Int): ModeDataClass?
+    suspend fun getAllModes(): List<ModeDataClass>?
 }
