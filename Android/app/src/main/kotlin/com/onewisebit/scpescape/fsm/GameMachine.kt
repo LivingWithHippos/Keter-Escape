a0570adbfbc823fb56be1cba286b538d8723cc6d
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-20 17:52:21 +0100

    Update GameMachine.kt
    added lists of necessary things

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/GameMachine.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/GameMachine.kt
@@ -1,8 +1,26 @@
 package com.onewisebit.scpescape.fsm
 
+import com.onewisebit.scpescape.model.entities.Participant
 import com.onewisebit.scpescape.model.entities.Player
+import com.onewisebit.scpescape.model.entities.Round
+import com.onewisebit.scpescape.model.entities.Turn
+import io.reactivex.Flowable
+import io.reactivex.Single
 
-class GameMachine(_players: List<Player> = emptyList()) {
-    var players: List<Player> = _players
-    var currentPlayer : Player? = null
+class GameMachine(gameStatePresenter: GameStateContract.GameStatePresenter) {
+    val presenter: GameStateContract.GameStatePresenter = gameStatePresenter
+
+    val participants: Flowable<List<Participant>>
+    val players: Single<List<Player>>
+    val rounds: Flowable<List<Round>>
+    val turns: Flowable<List<Turn>>
+
+    init {
+        participants= presenter.getParticipants()
+        players= presenter.getPlayers()
+        rounds = presenter.getRounds()
+        turns= presenter.getTurns()
+    }
+
+
 }
