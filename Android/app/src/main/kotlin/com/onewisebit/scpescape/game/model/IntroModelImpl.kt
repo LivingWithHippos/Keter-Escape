75024ae7ee4f674e9ee88599ad2b974298759135
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-21 23:39:29 +0100

    Replaced implementation with interface

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/game/model/IntroModelImpl.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/game/model/IntroModelImpl.kt
@@ -5,13 +5,14 @@ import com.onewisebit.scpescape.model.ModeDataClass
 import com.onewisebit.scpescape.model.entities.Game
 import com.onewisebit.scpescape.model.repositories.GameRepository
 import com.onewisebit.scpescape.model.repositories.InModeRepository
+import com.onewisebit.scpescape.model.repositories.InModelNewRepository
 import com.onewisebit.scpescape.model.repositories.ModeNewRepository
 import io.reactivex.Single
 
-class IntroModelImpl(gameRepository: GameRepository, modeRepository: ModeNewRepository) : IntroContract.IntroModel {
+class IntroModelImpl(gameRepository: GameRepository, modeRepository: InModelNewRepository) : IntroContract.IntroModel {
 
     private var gameRepo: GameRepository = gameRepository
-    private var modeRepo: ModeNewRepository = modeRepository
+    private var modeRepo: InModelNewRepository = modeRepository
 
     override suspend fun getGame(id: Long): Game {
         return gameRepo.getGameBlocking(id)
