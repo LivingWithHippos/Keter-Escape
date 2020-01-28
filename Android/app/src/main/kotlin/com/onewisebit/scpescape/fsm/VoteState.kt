69ce2a1a34793e5111ea7e2951038b3e05f9df27
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-28 20:57:45 +0100

    Added exception thrown

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/VoteState.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/VoteState.kt
@@ -18,7 +18,7 @@ class VoteState : GameState {
             choiceList.addAll(filterPlayers(participants, gameMachine.currentParticipant.playerID, rules.choiceEnabled as PlayersFilter))
         }
         else {
-            //TODO: add exception for unrecognized rules, add check for extends = vote
+            throw IllegalArgumentException("A vote state was requested but the input wasn't from a Vote Json")
         }
     }
 
