207df36dfd23ce97aacb273676d19bd3753420ae
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-28 11:48:50 +0100

    Temporary update to save these edits

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/VoteState.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/VoteState.kt
@@ -6,38 +6,43 @@ import com.onewisebit.scpescape.model.entities.Participant
 @SuppressLint("CheckResult")
 class VoteState : GameState {
 
-    override suspend fun handleInput(gameMachine: GameMachine, rules: HashMap<String, HashMap<String, Parameter>>) {
-        val participants : List<Participant> = gameMachine.participants
-        val candidatesList: HashSet<Long> = HashSet()
-        val showList: HashSet<Long> = HashSet()
-        val choiceList: HashSet<Long> = HashSet()
+    override suspend fun handleInput(gameMachine: GameMachine, rules: Input) {
+        if (rules is VoteJsonDataClass){
+            val participants : List<Participant> = gameMachine.participants
+            val candidatesList: HashSet<Long> = HashSet()
+            val showList: HashSet<Long> = HashSet()
+            val choiceList: HashSet<Long> = HashSet()
 
-        val showParameters = rules["show"]
-        candidatesList.addAll(filterPlayers(participants, gameMachine.currentParticipant.playerID, showParameters))
+            val showParameters = rules["show"]
+            candidatesList.addAll(filterPlayers(participants, gameMachine.currentParticipant.playerID, showParameters))
 
-        val revealParameters = rules["reveal_role"]
-        showList.addAll(filterPlayers(participants, gameMachine.currentParticipant.playerID, revealParameters))
+            val revealParameters = rules["reveal_role"]
+            showList.addAll(filterPlayers(participants, gameMachine.currentParticipant.playerID, revealParameters))
 
-        val choiceParameters = rules["choice_enabled"]
-        choiceList.addAll(filterPlayers(participants, gameMachine.currentParticipant.playerID, choiceParameters))
+            val choiceParameters = rules["choice_enabled"]
+            choiceList.addAll(filterPlayers(participants, gameMachine.currentParticipant.playerID, choiceParameters))
+        }
+        else {
+            //TODO: add exception for unrecognized rules, add check for extends = vote
+        }
 
     }
 
-    private fun filterPlayers(players : List<Participant>, currentParticipantId: Long, rules : HashMap<String, Parameter>?): HashSet<Long>{
+    private fun filterPlayers(players : List<Participant>, currentParticipantId: Long, rules : Parameter?): HashSet<Long>{
         val result: HashSet<Long> = HashSet()
-        if (rules?.get("all")?.getSingleBoolean() == true)
+        if (rules?.parameters?.firstOrNull { it.name == "all" }?.getSingleBoolean() == true)
             result.addAll(players.map { it.playerID })
-        if (rules?.get("self")?.getSingleBoolean() == true)
+        if (rules?.parameters?.firstOrNull { it.name == "self" }?.getSingleBoolean() == true)
             result.add(currentParticipantId)
         else
             result.remove(currentParticipantId)
-        if (!rules?.get("role")?.getStringList().isNullOrEmpty())
+        if (!rules?.parameters?.firstOrNull { it.name == "role" }?.getStringList().isNullOrEmpty())
             result.addAll(players.filter {
-                rules?.get("role")?.getStringList()?.contains(it.roleName) == true
+                rules?.parameters?.firstOrNull { role -> role.name == "role" }?.getStringList()?.contains(it.roleName) == true
             }.map { it.playerID })
-        if (!rules?.get("no_role")?.getStringList().isNullOrEmpty())
+        if (!rules?.parameters?.firstOrNull { it.name == "no_role" }?.getStringList().isNullOrEmpty())
             result.removeAll(players.filter {
-                rules?.get("no_role")?.getStringList()?.contains(it.roleName) == true
+                rules?.parameters?.firstOrNull { role -> role.name == "no_role" }?.getStringList()?.contains(it.roleName) == true
             }.map { it.playerID })
         return result
     }
