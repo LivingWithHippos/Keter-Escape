7bfd7f92040ab3b051fdab70f09efbd2dd523c5d
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-28 23:04:23 +0100

    Update Constants.kt
    added small category description
    removed unused constants

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Constants.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Constants.kt
@@ -3,25 +3,28 @@ package com.onewisebit.scpescape.utilities
 /**
  * Constants used throughout the app.
  */
+// file names
 const val DATABASE_NAME = "SCP.db"
 const val ROLE_DATA_FILENAME = "roles.json"
 const val MODE_DATA_FILENAME = "modes.json"
 const val ACTION_DATA_FILENAME = "actions.json"
 const val ACTION_TEMPLATE_FILENAME = "actions_template.json"
 
+// preferences and themes
 const val PREF_FILE = "scpescape_preferences"
 const val DEFAULT_THEME = "theme_foundation"
 const val CURRENT_THEME = "preference_current_theme"
 
-
+// game values
 const val GAME_TYPE = "game_type"
 const val GAME_TYPE_PASS = 0
 const val GAME_TYPE_LOCAL = 1
 const val GAME_TYPE_NET = 2
 
-const val GAME_CLASSIC_MIN_PLAYERS = 7
-const val GAME_CLASSIC_MID_PLAYERS = 12
-const val GAME_CLASSIC_MAX_PLAYERS = 30
-
 const val PLAYER_STATE_DEAD = 0
 const val PARTICIPANT_STATE_ALIVE = 1
+
+// Classic mode values
+const val DAY = 0
+const val NIGHT = 1
+
