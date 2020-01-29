af7e9c257104a89f3e74cf5936558a1a030c746b
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-29 23:20:03 +0100

    removed unused extension function

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Extensions.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Extensions.kt
@@ -6,11 +6,6 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.annotation.LayoutRes
 
-//TODO: check if it can be converted, unused at the moment
-fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
-    return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
-}
-
 fun View.hideKeyboard() {
     val inputMethodManager =
         context!!.getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as? InputMethodManager
