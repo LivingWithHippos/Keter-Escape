c4815e2959887e640e2468797a11024f8d49b1d9
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-01-30 11:14:43 +0100

    updated todo

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/game/view/DayNightFragment.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/game/view/DayNightFragment.kt
@@ -2,5 +2,6 @@ package com.onewisebit.scpescape.game.view
 
 import com.onewisebit.scpescape.fsm.actions.Action
 
-class DayNightFragment(gameID: Long, private val onActionListener: (action: Action) -> Unit) : BaseGameFragment(gameID, onActionListener) {
+//TODO: rename this to RoundSwitchFragment. This fragment will be shown between rounds an will describe what is going to happen in the next round. Also rename the other day/light pieces.
+class DayNightFragment(gameID: Long, onActionListener: (action: Action) -> Unit) : BaseGameFragment(gameID, onActionListener), {
 }
