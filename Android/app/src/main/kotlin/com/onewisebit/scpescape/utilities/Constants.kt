dcd12e2618dcfc993b0d5243e53aca21fd31cd1b
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-08 15:49:39 +0100

    Reworked json files structure
    renamed files
    added folders
    changed inner json structure

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Constants.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Constants.kt
@@ -3,34 +3,50 @@ package com.onewisebit.scpescape.utilities
 /**
  * Constants used throughout the app.
  */
-// file names
+
+// db files
+
 const val DATABASE_NAME = "SCP.db"
-const val ROLE_DATA_FILENAME = "roles.json"
-const val MODE_DATA_FILENAME = "modes.json"
-const val ACTION_DATA_FILENAME = "actions.json"
-const val ACTION_TEMPLATE_FILENAME = "actions_template.json"
-const val ROUND_DETAILS_FILENAME = "round_details.json"
+
+// assets files and folders
+
+const val TEMPLATE_FOLDER = "template/"
+const val TEMPLATE_FILE_VOTE = "info.json"
+const val TEMPLATE_FILE_INFO = "vote.json"
+
+const val MODE_file = "mode.json"
+const val ACTION_FOLDER = "actions/"
+const val ACTION_FILE = "actions.json"
+const val ROLE_FOLDER = "roles/"
+const val ROLE_FILE = "roles.json"
+const val ROUND_FOLDER = "rounds/"
+const val ROLE_DETAILS_FILE = "rounds.json"
 
 // shared preferences
+
 const val PREF_FILE = "scpescape_preferences"
 const val DEFAULT_THEME = "theme_foundation"
 const val CURRENT_THEME = "preference_current_theme"
 
 // game values
+
 const val GAME_TYPE = "game_type"
 const val GAME_TYPE_PASS = 0
 const val GAME_TYPE_LOCAL = 1
 const val GAME_TYPE_NET = 2
 
 // game states
+
 const val PARTICIPANT_STATE_DEAD = 0
 const val PARTICIPANT_STATE_ALIVE = 1
 
 // Classic mode values
+
 const val DAY = 0
 const val NIGHT = 1
 
 // Bundle arguments
+
 const val ARG_TURN_NUMBER = "turn_number"
 const val ARG_PLAYER_NAME = "player_name"
 
