3a5e554e38626702cc98795f6dfe19a1abcdc0f8
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-10 17:04:35 +0100

    updated error message

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/modesList/model/GameModesModelImpl.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/modesList/model/GameModesModelImpl.kt
@@ -3,14 +3,14 @@ package com.onewisebit.scpescape.modesList.model
 import com.onewisebit.scpescape.model.parsed.ModeDataClass
 import com.onewisebit.scpescape.model.repositories.InModeJSONRepository
 import com.onewisebit.scpescape.modesList.GameModesContract
-import com.onewisebit.scpescape.utilities.MODE_DATA_FILENAME
 
 class GameModesModelImpl(modeRepository: InModeJSONRepository) : GameModesContract.GameModesModel {
 
     private var modeRepo: InModeJSONRepository = modeRepository
 
+    //todo: check error in called method
     override suspend fun getModes(): List<ModeDataClass> {
         return modeRepo.getAllModes()
-            ?: throw IllegalArgumentException("No modes could be loaded from $MODE_DATA_FILENAME")
+            ?: throw IllegalArgumentException("No modes could be loaded")
     }
 }
