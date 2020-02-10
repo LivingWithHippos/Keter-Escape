c00c35faf04023edd43bfb0d735bd1ec7d3e2958
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-10 18:33:19 +0100

    updated roleworker with new assets structure

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/workers/PopulateDatabaseRolesWorker.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/workers/PopulateDatabaseRolesWorker.kt
@@ -8,8 +8,14 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonReader
 import com.onewisebit.scpescape.model.database.SCPDatabase
+import com.onewisebit.scpescape.model.entities.Mode
 import com.onewisebit.scpescape.model.entities.Role
-import com.onewisebit.scpescape.utilities.ROLE_DATA_FILENAME
+import com.onewisebit.scpescape.model.parsed.ModeDataClass
+import com.onewisebit.scpescape.model.parsed.RoleDetails
+import com.onewisebit.scpescape.model.repositories.JSONRepository
+import com.onewisebit.scpescape.utilities.MODE_FILE
+import com.onewisebit.scpescape.utilities.ROLE_FILE
+import com.onewisebit.scpescape.utilities.ROLE_FOLDER
 import kotlinx.coroutines.coroutineScope
 
 /**
@@ -19,23 +25,50 @@ class PopulateDatabaseRolesWorker(
     context: Context,
     workerParams: WorkerParameters
 ) : CoroutineWorker(context, workerParams) {
-    override suspend fun doWork(): Result = coroutineScope {
-        try {
-            // Populating roles
-            applicationContext.assets.open(ROLE_DATA_FILENAME).use { inputStream ->
-                JsonReader(inputStream.reader()).use { jsonReader ->
-                    val roleType = object : TypeToken<List<Role>>() {}.type
-                    val rolesList: List<Role> = Gson().fromJson(jsonReader, roleType)
 
-                    val database = SCPDatabase.getInstance(applicationContext)
-                    database.roleDAO().insertAll(rolesList)
+    private val repo = JSONRepository(context)
+
+    override suspend fun doWork(): Result = coroutineScope {
+        // leggi modalità
+        // leggi ruoli
+        // mappa e popola db
+
+        val modePaths: MutableList<String> = repo.searchFile(MODE_FILE, maxDepth = 2)
+        val database = SCPDatabase.getInstance(applicationContext)
+
+        for (path in modePaths) {
+            val modeDetails: MutableList<ModeDataClass> = mutableListOf()
+            try {
+                applicationContext.assets.open(path).use { inputStream ->
+                    JsonReader(inputStream.reader()).use { jsonReader ->
+                        val modeType = object : TypeToken<List<ModeDataClass>>() {}.type
+                        val added = modeDetails.addAll(Gson().fromJson(jsonReader, modeType))
+                    }
                 }
-                Result.success()
+            } catch (ex: Exception) {
+                Log.e(TAG, "Error getting the mode from path $path", ex)
+                Result.failure()
             }
-        } catch (ex: Exception) {
-            Log.e(TAG, "Error populating the database", ex)
-            Result.failure()
+
+            val rolePath = path.removeSuffix(MODE_FILE).plus(ROLE_FOLDER).plus(ROLE_FILE)
+
+            try {
+                applicationContext.assets.open(rolePath).use { inputStream ->
+                    JsonReader(inputStream.reader()).use { jsonReader ->
+                        val roleType = object : TypeToken<List<RoleDetails>>() {}.type
+                        val rolesList: List<RoleDetails> = Gson().fromJson(jsonReader, roleType)
+                        val modeId = modeDetails[0].id
+                        database.roleDAO().insertAll(rolesList.map { Role(modeId,it.name,it.description,it.group) })
+                    }
+                    Result.success()
+                }
+            } catch (ex: Exception) {
+                Log.e(TAG, "Error populating the role database for path $path", ex)
+                Result.failure()
+            }
         }
+
+        Result.success()
     }
 
     companion object {
