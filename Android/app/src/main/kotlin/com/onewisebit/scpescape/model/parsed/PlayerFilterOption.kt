4ad8c2ff00a64ad16c530645fe270b0ef4c080e8
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-10 16:01:54 +0100

    remove unused interface

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/model/parsed/PlayerFilterOption.kt
+++
@@ -1,8 +0,0 @@
-package com.onewisebit.scpescape.model.parsed
-
-abstract class PlayerFilterOption {
-    abstract val all: Boolean
-    abstract val self: Boolean
-    abstract val role: List<String>
-    abstract val noRole: List<String>
-}
