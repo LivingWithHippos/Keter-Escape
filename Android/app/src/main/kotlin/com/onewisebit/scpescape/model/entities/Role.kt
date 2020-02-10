76776ca2a605fac5d322b11fdc6d1ceb36918f5b
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-10 18:33:12 +0100

    Updated Role entity with mode id

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/model/entities/Role.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/model/entities/Role.kt
@@ -1,13 +1,20 @@
 package com.onewisebit.scpescape.model.entities
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
 
-@Entity(tableName = "roles")
+@Entity(tableName = "roles",
+        primaryKeys = ["mode", "role_name"],
+    foreignKeys = [ForeignKey(
+        entity = Mode::class,
+        parentColumns = ["mode_ID"],
+        childColumns = ["mode"],
+        onDelete = ForeignKey.CASCADE)
+    ],
+    indices = [Index(value = ["role_name"], unique = true)]
+)
 class Role(
-    //TODO: maybe a numerical id would be better
-    @PrimaryKey
+    @ColumnInfo(name = "mode")
+    val modeId: Int,
     @ColumnInfo(name = "role_name")
     val name: String,
     @ColumnInfo(name = "role_description")
