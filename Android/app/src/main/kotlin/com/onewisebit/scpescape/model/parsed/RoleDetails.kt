818472d0adcba0c4eb39e6c26f1b8452bb921750
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-10 16:02:30 +0100

    Create RoleDetails.kt
    data class for role.json

---
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/model/parsed/RoleDetails.kt
@@ -0,0 +1,21 @@
+package com.onewisebit.scpescape.model.parsed
+import com.google.gson.annotations.SerializedName
+
+
+data class RoleDetails(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("group")
+    val group: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("round_actions")
+    val roundActions: List<RoundAction>
+)
+
+data class RoundAction(
+    @SerializedName("round_code")
+    val roundCode: String,
+    @SerializedName("action_name")
+    val actionName: String
+)
