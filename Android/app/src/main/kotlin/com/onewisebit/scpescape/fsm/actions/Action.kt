f223070a75b2628304d81e27135888c5b8d64288
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-11 10:54:22 +0100

    added actions to fsm

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/actions/Action.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/actions/Action.kt
@@ -3,10 +3,12 @@ package com.onewisebit.scpescape.fsm.actions
 sealed class Action {
     class EndTurnClicked : Action()
     class PassedToPlayerClicked : Action()
+    class PlayTurnClicked : Action()
     class VoteCasted : Action()
     class ResultSeenClicked : Action()
     class EndRoundClicked : Action()
     class StartVotePowerClicked : Action()
+    class StartInfoPowerClicked : Action()
     class StartRoundClicked : Action()
     class StartGameClicked : Action()
 }
