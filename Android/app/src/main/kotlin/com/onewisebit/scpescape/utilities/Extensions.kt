1c4fdbbdca5458359ea0c880cf94de1d9bed505f
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-12 21:48:59 +0100

    added stack-like pop function to MutableList

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Extensions.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Extensions.kt
@@ -8,3 +8,12 @@ fun View.hideKeyboard() {
         context!!.getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as? InputMethodManager
     inputMethodManager?.hideSoftInputFromWindow(this.windowToken, 0)
 }
+
+fun <T> MutableList<T>.pop() : T? {
+    val item = lastOrNull()
+
+    if (!isEmpty()){
+        removeAt(size -1)
+    }
+    return item
+}
