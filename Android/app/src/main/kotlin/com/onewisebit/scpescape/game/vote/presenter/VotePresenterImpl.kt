008387a7be75d374241b11c62029c77033e62bbc
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-12 23:06:33 +0100

    Update VotePresenterImpl.kt

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/game/vote/presenter/VotePresenterImpl.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/game/vote/presenter/VotePresenterImpl.kt
@@ -2,17 +2,26 @@ package com.onewisebit.scpescape.game.vote.presenter
 
 import com.onewisebit.scpescape.game.vote.VoteContract
 import com.onewisebit.scpescape.game.composable.ContractAction
+import com.onewisebit.scpescape.game.composable.ContractParticipant
+import com.onewisebit.scpescape.game.composable.ContractVote
 import com.onewisebit.scpescape.model.entities.Participant
 import com.onewisebit.scpescape.model.entities.Player
 import com.onewisebit.scpescape.model.entities.Vote
+import com.onewisebit.scpescape.model.parsed.TurnAction
+import com.onewisebit.scpescape.model.parsed.VoteTurn
 
 class VotePresenterImpl(
     val view: VoteContract.VoteView,
+    val votePresenter:ContractVote.PresenterVote,
     val actionPresenter:ContractAction.PresenterAction,
+    val participantPresenter:ContractParticipant.PresenterParticipant,
     val gameID: Long,
     val roundCode : String,
     val roleName: String
-): VoteContract.VotePresenter, ContractAction.PresenterAction by actionPresenter {
+): VoteContract.VotePresenter,
+    ContractVote.PresenterVote by votePresenter,
+    ContractParticipant.PresenterParticipant by participantPresenter,
+    ContractAction.PresenterAction by actionPresenter {
 
     override suspend fun loadValues() {
 
