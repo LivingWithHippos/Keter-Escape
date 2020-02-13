b613d7258301cee37c72a93bce6a5fb20bd7abed
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-13 19:24:00 +0100

    Removed unnecessary Action classes from fsm

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/states/PlayerTurnState.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/states/PlayerTurnState.kt
@@ -7,8 +7,6 @@ class PlayerTurnState : StateGame {
         return when (action) {
             is Action.EndRoundClicked -> ShowResultsState()
             is Action.EndTurnClicked -> PassDeviceState()
-            is Action.StartVotePowerClicked -> TurnVoteState()
-            is Action.StartInfoPowerClicked -> TurnInfoState()
             is Action.PlayTurnClicked -> PlayerPowerState()
             //todo: replace all these with illegal argument?
             else -> throw IllegalStateException("Invalid action: $action passed to a PlayerTurn state")
