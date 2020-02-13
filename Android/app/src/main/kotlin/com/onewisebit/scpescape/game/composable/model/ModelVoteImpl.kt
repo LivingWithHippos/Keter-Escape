1602250d624469c5e4842147ffecc1e35be31d30
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-02-13 11:29:50 +0100

    Formatted code

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/game/composable/model/ModelVoteImpl.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/game/composable/model/ModelVoteImpl.kt
@@ -4,7 +4,7 @@ import com.onewisebit.scpescape.game.composable.ContractVote
 import com.onewisebit.scpescape.model.entities.Vote
 import com.onewisebit.scpescape.model.repositories.InVoteRepository
 
-class ModelVoteImpl(val voteRepository: InVoteRepository): ContractVote.ModelVote {
+class ModelVoteImpl(val voteRepository: InVoteRepository) : ContractVote.ModelVote {
 
     override suspend fun getGameVotes(gameId: Long): List<Vote> =
         voteRepository.getGameVotes(gameId)
@@ -12,7 +12,8 @@ class ModelVoteImpl(val voteRepository: InVoteRepository): ContractVote.ModelVot
     override suspend fun getRoundVotes(gameId: Long, roundNumber: Int): List<Vote> =
         voteRepository.getRoundVotes(gameId, roundNumber)
 
-    override suspend fun getLastRoundVotes(gameId: Long): List<Vote> = voteRepository.getLastRoundVotes(gameId)
+    override suspend fun getLastRoundVotes(gameId: Long): List<Vote> =
+        voteRepository.getLastRoundVotes(gameId)
 
     override suspend fun addVote(gameId: Long, roundNumber: Int,turnNumber: Int,playerID: Long,votedPlayerID: Long,voteType: String) =
         voteRepository.insertVote(Vote(0,gameId,roundNumber,turnNumber,playerID,votedPlayerID,voteType))
