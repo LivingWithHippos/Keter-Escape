2ce2c5cb51eb3e429fbc43efcca4af330af8a1cf
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-03-10 08:56:03 +0100

    Use let to avoid null errors

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Extensions.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/utilities/Extensions.kt
@@ -4,9 +4,10 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun View.hideKeyboard() {
-    val inputMethodManager =
-        context!!.getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-    inputMethodManager?.hideSoftInputFromWindow(this.windowToken, 0)
+    context?.let{
+        val inputMethodManager = it.getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(this.windowToken, 0)
+    }
 }
 
 /**
