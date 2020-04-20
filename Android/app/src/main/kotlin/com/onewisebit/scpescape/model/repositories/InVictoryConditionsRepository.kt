2462eaa135d479be3a3dc42a13a698a2f1984581
LivingWithHippos <salabracco91@gmail.com>
2020-04-20 16:15:16 +0200

    added InVictoryConditionsRepository

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/model/repositories/InVictoryConditionsRepository.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/model/repositories/InVictoryConditionsRepository.kt
@@ -1,4 +1,7 @@
 package com.onewisebit.scpescape.model.repositories
 
+import com.onewisebit.scpescape.model.parsed.VictoryCondition
+
 interface InVictoryConditionsRepository {
+    suspend fun getVictoryConditions(modeID: Int): List<VictoryCondition>
 }
