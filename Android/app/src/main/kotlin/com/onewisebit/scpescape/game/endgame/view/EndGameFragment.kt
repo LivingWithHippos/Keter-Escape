84023fc6d90077329a026800a0719315d20b72cc
LivingWithHippos <salabracco91@gmail.com>
2020-04-20 19:28:33 +0200

    Update EndGameFragment
    load the winning message

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/game/endgame/view/EndGameFragment.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/game/endgame/view/EndGameFragment.kt
@@ -6,51 +6,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.onewisebit.scpescape.R
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.onewisebit.scpescape.databinding.FragmentEndGameBinding
+import com.onewisebit.scpescape.databinding.FragmentRoundResultBinding
+import com.onewisebit.scpescape.fsm.actions.Action
+import com.onewisebit.scpescape.game.BaseGameFragment
+import com.onewisebit.scpescape.utilities.ARG_KILLED_PLAYERS
+import com.onewisebit.scpescape.utilities.ARG_WINNING_MESSAGE
+
 
 /**
  * A simple [Fragment] subclass.
  * Use the [EndGameFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class EndGameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class EndGameFragment(gameID: Long, private val onActionListener: (action: Action) -> Unit) :
+    BaseGameFragment<FragmentEndGameBinding>(gameID, onActionListener) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentEndGameBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {args ->
+            args.getString(ARG_WINNING_MESSAGE)?.let { initFragment(it) }
         }
+
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_end_game, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EndGameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                EndGameFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    private fun initFragment(message: String) {
+        binding.tvMessage.text = message
     }
 }
