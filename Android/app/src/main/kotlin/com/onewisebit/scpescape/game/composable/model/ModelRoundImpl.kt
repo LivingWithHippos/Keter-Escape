2cbb5445e3a4508fd537e725460615be8c9eea9f
LivingWithHippos <salabracco91@gmail.com>
2020-04-22 18:20:23 +0200

    removed wrong round number assignment

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/game/composable/model/ModelRoundImpl.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/game/composable/model/ModelRoundImpl.kt
@@ -1,5 +1,7 @@
 package com.onewisebit.scpescape.game.composable.model
 
+import android.util.Log
+import com.onewisebit.scpescape.di.SCPApplication
 import com.onewisebit.scpescape.game.composable.ContractRound
 import com.onewisebit.scpescape.model.entities.Round
 import com.onewisebit.scpescape.model.parsed.RoundDetails
@@ -20,17 +22,22 @@ class ModelRoundImpl(val roundRepository: InRoundRepository) :
     override suspend fun getRoundsMode(gameId: Long): Int = roundRepository.getRoundsMode(gameId)
 
     override suspend fun addRound(gameID: Long, details: String, roundNumber: Int) {
-        var number = roundNumber
-
+        var number: Int = roundNumber
         if (number < 0) {
+            // if roundNumber is less than zero we check how many rows are in the Round table (hehe) for this game
+            //todo: rename method to getRoundsRows?
             number = roundRepository.getRoundsNumber(gameID)
-            if (number > 0)
-                number += 1
+            // since getRoundsNumber returns the number of rows we can use that number directly
+            // 0 rows = start of the game, insert zero for the first round
+            // 1 row = second round, insert 1 as round number
+            // 2 rows = third round, insert 2 as round number
+            // etc.
         }
 
         val mode = getRoundsMode(gameID)
 
-        roundRepository.insertRound(Round(number, gameID, mode, details))
+        val row = roundRepository.insertRound(Round(number, gameID, mode, details))
+        Log.d(TAG, "Inserted Round's row is $row")
     }
 
     /**
