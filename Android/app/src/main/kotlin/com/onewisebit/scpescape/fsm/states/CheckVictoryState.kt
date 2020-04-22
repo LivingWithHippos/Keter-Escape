eb84394ead46af38439c2017726d3feec1320891
LivingWithHippos <salabracco91@gmail.com>
2020-04-22 16:08:51 +0200

    Fix fsm state

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/states/CheckVictoryState.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/states/CheckVictoryState.kt
@@ -7,7 +7,7 @@ import com.onewisebit.scpescape.utilities.NIGHT
 class CheckVictoryState : StateGame {
     override fun consumeAction(action: Action): StateGame {
         return when (action) {
-            is Action.VictoryReached -> PlayerTurnState()
+            is Action.VictoryReached -> EndGameState()
             is Action.StartDayRoundClicked -> RoundInfoState(DAY)
             is Action.StartNightRoundClicked -> RoundInfoState(NIGHT)
             else -> throw IllegalGameStateTransition("Invalid action: $action passed to a PassDevice state")
