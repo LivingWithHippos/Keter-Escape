540464e93d747f4fb12c12449006b6f4af2594e5
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-04-30 18:44:49 +0200

    Added name related functions to StateGame

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/states/StateGame.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/fsm/states/StateGame.kt
@@ -4,6 +4,15 @@ import com.onewisebit.scpescape.fsm.actions.Action
 
 interface StateGame {
     fun consumeAction(action: Action): StateGame
+
+    fun confrontName(name: String): Boolean {
+        return getName().equals(name, ignoreCase = true)
+    }
+
+    //todo: check if this returns the correct class or the interface
+    fun getName(): String {
+        return this::class.java.simpleName
+    }
 }
 
 class IllegalGameStateTransition(message: String) : IllegalStateException(message)
