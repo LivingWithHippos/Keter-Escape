4d7cae96021bb299db758b7137a611a8e816ab30
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-05-03 00:56:04 +0200

    added state to database

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/model/database/SCPDatabase.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/model/database/SCPDatabase.kt
@@ -18,7 +18,7 @@ import com.onewisebit.scpescape.workers.PopulateDebugDataWorker
  * The Room database that contains the Users table
  */
 @Database(
-    entities = [Player::class, Role::class, Game::class, Mode::class, Participant::class, Round::class, Turn::class, Vote::class, Save::class],
+    entities = [Player::class, Role::class, Game::class, Mode::class, Participant::class, Round::class, Turn::class, Vote::class, Save::class, State::class],
     version = 1
 )
 abstract class SCPDatabase : RoomDatabase() {
