d1b02d99f09014578cd12257ecdede5d4d21c157
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-06-11 17:16:59 +0200

    removed unused overriding method

--- Android/app/src/main/kotlin/com/onewisebit/scpescape/main/loadgames/view/GameListFragment.kt
+++ Android/app/src/main/kotlin/com/onewisebit/scpescape/main/loadgames/view/GameListFragment.kt
@@ -25,10 +25,6 @@ class GameListFragment : BaseSCPFragment<FragmentGameListBinding>(),
     private lateinit var adapter: GameListAdapter
     private val presenter: GamesListContract.GamesListPresenter by inject { parametersOf(this) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
